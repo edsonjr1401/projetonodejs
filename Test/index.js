123570c8faf842eabae6ed92476cc99d9a0fbbac
Edson Junior <edsoon.jr1401@gmail.com>
2025-09-18 13:37:22 -0300

    att

--- Test/index.js
+++ Test/index.js
@@ -1,23 +1,13 @@
 const express = require("express")
 const app = express();
+const handlebars = require('express-handlebars')
 
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname+ "/html/index.html");
-});
+ //Config
+   //Templete Engine
+     app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    app.set('view handlebars.engine', 'hanldebars')
 
-app.get('/sobre', function(req, res){
-    res.sendFile(__dirname + "/html/sobre.html");
-})
-
-app.get('/blog', function(req, res){
-    res.sendFile(__dirname + "/html/blog.html");
-})
-
-app.get('/ola/:cargo/:nome/:cor', function(req, res){
-    res.send("<h1>Ola "+req.params.nome+"</h1>"+"<h1>Sua cor favorita é: "+req.params.cor+"</h3>"+"<h2>Seu cargo é: "+req.params.cargo+"</h2");
-  
-})
 
 
 app.listen(8081, function(){
