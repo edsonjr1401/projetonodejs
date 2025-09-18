9391f0bed1700b815d713d99df407ff8531e556c
Edson Junior <edsoon.jr1401@gmail.com>
2025-09-18 15:48:04 -0300

    att

--- Test/html/teste.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO usuarios(nome, email, idade) VALUES (
-    "Miranha",
-    "vaiteia@gmail.com",
-    19
-);
--- Test/index.js
+++ Test/index.js
@@ -1,14 +1,25 @@
-const express = require("express")
+const express = require("express"); // <- IMPORTAR O EXPRESS
 const app = express();
-const handlebars = require('express-handlebars')
+const { engine } = require('express-handlebars')
+const Sequelize = require('sequelize')
 
 
  //Config
    //Templete Engine
-     app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-    app.set('view handlebars.engine', 'hanldebars')
+    app.engine('handlebars', engine({ defaultLayout: 'main' }));
+    app.set('view engine', 'handlebars')
+    
+    // Conexão com o banco de dados Mysql
+    const sequelize = new Sequelize('test', 'root', '6863', {
+    host: "localhost",
+    dialect: 'mysql'
+})
 
+//ROTAS
 
+    app.get('/cad', function(req, res){
+    res.render("formulario")
+})
 
 app.listen(8081, function(){
     console.log("Serveidor Rodando!");
