91b5c3bb9131c5ee3c9ea105b65111795f105bed
Edson Junior <edsoon.jr1401@gmail.com>
2025-09-23 16:35:07 -0300

    att

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express"); // <- IMPORTAR O EXPRESS
 const app = express();
 const { engine } = require('express-handlebars');
 const bodyParser = require('body-parser')
-const Post = require('./models/Post')
+const Post = require('./models/Post');
+const { where } = require("sequelize");
 
 Post.sync({force: false})
 
@@ -47,6 +48,12 @@ Post.sync({force: false})
       }).catch(function(erro){
          res.send("Houve um erro: " + erro)
       })
+})
+
+app.get('/deletar/:id', function(req, res){
+  Post.destroy({where: {'id': req.params.id}}).then(function(){
+    res.send("Postagem deletada com sucesso!")
+  })
 })
 
 app.listen(8081, function(){
